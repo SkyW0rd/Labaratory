6501a66d8ee4e96851173094b3642aea9fcbe812
SkyW0rd <101702769+SkyW0rd@users.noreply.github.com>
2022-04-28 23:16:46 +0300

    Create VectorStack.h

---
+++ stack/VectorStack.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include "StackImplementation.h"
+
+using ValueType = double;
+
+class VectorStack: public IStackImplementation
+{
+    public:
+        VectorStack();
+ 
+        void push(const ValueType& value) override;
+        void pop() override;
+        const ValueType& top() const override;
+        bool isEmpty() const override;
+        size_t size() const override;
+ 
+        ~VectorStack();
+    private:
+        std::vector<ValueType> _v;
+};
