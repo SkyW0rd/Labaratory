c52165f719c3469fd7e89009ce6b021beb357cc6
SkyW0rd <101702769+SkyW0rd@users.noreply.github.com>
2022-04-28 23:19:53 +0300

    Create ListStack.h

---
+++ stack/ListStack.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <list>
+#include <iostream>
+#include "StackImplementation.h"
+
+using ValueType = double;
+
+class ListStack: public IStackImplementation
+{
+    public:
+        ListStack();
+        
+        void push(const ValueType& value) override;
+        void pop() override;
+        const ValueType& top() const override;
+        bool isEmpty() const override;
+        size_t size() const override;
+    
+        ~ListStack();
+    private:
+        std::list<ValueType> _l;
+};
