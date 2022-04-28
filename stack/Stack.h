a8ba8c6ca319cf0b60405378d11735839f8215fb
SkyW0rd <101702769+SkyW0rd@users.noreply.github.com>
2022-04-28 23:17:41 +0300

    Update Stack.h

--- stack/Stack.h
+++ stack/Stack.h
@@ -7,34 +7,32 @@
 using ValueType = double;
 
 enum class StackContainer{
-  Vector = 0,
-  List,
+    Vector = 0,
+    List,
 };
 
 class IStackImplementation;
 
 class Stack
 {
-  public:
-
-    Stack(StackContainer container = StackContainer::Vector);
-    Stack(const ValueType* ValueArray, const size_t arraySize, StackContainer container = StackContainer::Vector);
-
-    explicit Stack(const Stack& copyStack);
-    Stack& operator=(const Stack& copyStack);
-
-    Stack(Stack&& moveStack) noexcept;
-    Stack& operator=(Stack&& moveStack) noexcept;
-
-    ~Stack();
-
-    void push(const ValueType& value);
-    void pop();
-    size_t size() const;
-    bool isEmpty() const;
-    const ValueType& top() const;
-
-  private:
-    IStackImplementation* _pimpl = nullptr;
-    StackContainer _containerType;
+    public:
+        Stack(StackContainer container = StackContainer::Vector);
+        Stack(const ValueType* ValueArray, const size_t arraySize, StackContainer container = StackContainer::Vector);
+    
+        explicit Stack(const Stack& copyStack);
+        Stack& operator=(const Stack& copyStack);
+    
+        Stack(Stack&& moveStack) noexcept;
+        Stack& operator=(Stack&& moveStack) noexcept;
+    
+        ~Stack();
+    
+        void push(const ValueType& value);
+        void pop();
+        size_t size() const;
+        bool isEmpty() const;
+        const ValueType& top() const;
+    private:
+        IStackImplementation* _pimpl = nullptr;
+        StackContainer _containerType;
 };
