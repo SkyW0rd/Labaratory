375dc6a1a085dcd97976a2bef3f79c9068be068d
SkyW0rd <101702769+SkyW0rd@users.noreply.github.com>
2022-04-29 21:50:17 +0300

    Update VectorStack.cpp

--- stack/VectorStack.cpp
+++ stack/VectorStack.cpp
@@ -1,46 +1,32 @@
 #include "VectorStack.h"
-VectorStack::VectorStack()
-{
-}
-
 void VectorStack::push(const ValueType& value)
 {
-    _v.push_back(value);
+  _v.push_back(value);
 }
 void VectorStack::pop()
 {
-    if (_v.size() != 0)
-    {
-        _v.pop_back();
-    }
+    _v.pop_back();
 }
 const ValueType& VectorStack::top() const
 {
-    if(_v.size() != 0)
-    {
-        return _v.back();
-    }
-    else
-    {
-        return -1;
-    }
+  return _v.back();
 }
 bool VectorStack::isEmpty() const
 {
-    if (_v.size() == 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+  if (_v.size() == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 size_t VectorStack::size() const
 {
-    return _v.size();
+  return _v.size();
 }
 VectorStack::~VectorStack()
 {
-    _v.clear();
+  _v.clear();
 }
