c54e6a2f6ae6b718e39ed98db6eb3e3af42e08db
SkyW0rd <101702769+SkyW0rd@users.noreply.github.com>
2022-04-29 21:50:02 +0300

    Update ListStack.cpp

--- stack/ListStack.cpp
+++ stack/ListStack.cpp
@@ -1,45 +1,33 @@
 #include "ListStack.h"
-ListStack::ListStack()
-{
-}
+
 void ListStack::push(const ValueType& value)
 {
-    _l.push_back(value);
+  _l.push_back(value);
 }
 void ListStack::pop()
 {
-    if (_l.size() != 0)
-    {
-        _l.pop_back();
-    }
+    _l.pop_back();
 }
 const ValueType& ListStack::top() const
 {
-    if (_l.size() != 0)
-    {
-        return _l.back();
-    }
-    else
-    {
-        return -1;
-    }
+    return _l.back();
 }
 bool ListStack::isEmpty() const
 {
-    if (_l.size() == 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+  if (_l.size() == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 size_t ListStack::size() const
 {
-    return _l.size();
+  return _l.size();
 }
 ListStack::~ListStack()
 {
-    _l.clear();
+  _l.clear();
 }
