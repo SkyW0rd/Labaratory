52118b994bc741248d8150290415776818befbee
SkyW0rd <101702769+SkyW0rd@users.noreply.github.com>
2022-06-13 19:30:15 +0300

    Update BST.h

--- BST/BST.h
+++ BST/BST.h
@@ -1,68 +1,218 @@
 #pragma once
-
-#include <utility>
 #include <cstddef>
-#include <iosfwd>
 #include <iostream>
-#include <algorithm>
-#include <cassert>
-#include <cstdio>
+#include <utility>
+#include <iosfwd>
 #include <stdexcept>
+/*!
+    Имплементация бинарного дерева поиска
+    Допускается дублирование ключей (аналог multimap)
+*/
+template <typename Key, typename Value>
+class BinarySearchTree
+{
+    struct Node
+    {
+        Node(Key key, 
+             Value value, 
+             Node* parent = nullptr, 
+             Node* left = nullptr, 
+             Node* right = nullptr);
 
-using KeyType = std::string;
-using ValueType = double;
-struct Node
-{
-    KeyType key;
-    ValueType value;
-    Node* left;
-    Node* right;
-    Node* parent;
-};
-class Iterator
-{
+        std::pair<Key, Value> keyValuePair;
+        Node* parent = nullptr;
+        Node* left = nullptr;
+        Node* right = nullptr;
+    };
+void add(Node* pref, Node* current);
+void _print(Node* pref, int level = 0);
+void _erase(Node* pref, Key& key);
+Node* leftbranch(Node* node);
+Node* rightbranch(Node* node);
 public:
-    explicit Iterator(Node* ptr): _ptr(ptr){}
-    std::pair<KeyType, ValueType>& operator*();
-    const std::pair<KeyType, ValueType>& operator*() const;
-    Node* operator->();
-    const Node* operator->() const;
-    Iterator operator++();
-    Iterator operator++(int);
-    bool operator==(const Iterator& other) const;
-    bool operator!=(const Iterator& other) const;
-
-private:
-    Node* _ptr;
-};
-
-class BST
-{
-public:
-
-    BST() = default;
-
-    explicit BST(const BST& other);
-    BST& operator=(const BST& other);
-
-    explicit BST(BST&& other) noexcept;
-    BST& operator=(BST&& other) noexcept;
-
-    ~BST();
+    //! Конструктор по умолчанию
+    BinarySearchTree() = default;
     
-    void insert(KeyType key, ValueType value);
-    void remove(KeyType key);
-    ValueType& find(const KeyType& key);
-    const ValueType& find(const KeyType& key) const;
-    Node* min(KeyType key) const;
-    Node* max(KeyType key) const;
-    Iterator root();
+    //! Копирование
+    explicit BinarySearchTree(const BinarySearchTree& other);
+    BinarySearchTree& operator=(const BinarySearchTree& other);
+    //! Перемещение
+    explicit BinarySearchTree(BinarySearchTree&& other) noexcept;
+    BinarySearchTree& operator=(BinarySearchTree&& other) noexcept;
+
+    //! Деструктор
+    ~BinarySearchTree();
+
+    /*!
+        Итератор бинарного дерева поиска
+
+        Обходит дерево последовательно от меньшего ключа к большему 
+    */
+    class Iterator
+    {
+    public:
+        explicit Iterator(Node* node);
+
+        std::pair<Key, Value>& operator*();
+        const std::pair<Key, Value>& operator*() const;
+
+        std::pair<Key, Value>* operator->();
+        const std::pair<Key, Value>* operator->() const;
+
+        Iterator operator++();
+        Iterator operator++(int);
+
+        Iterator operator--();
+        Iterator operator--(int);
+
+        bool operator==(const Iterator& other) const;
+        bool operator!=(const Iterator& other) const;
+
+    private:
+        Node* _node;
+    };
+
+    /*!
+        Константный итератор
+    */
+    class ConstIterator
+    {
+    public:
+        explicit ConstIterator(const Node* node);
+
+        const std::pair<Key, Value>& operator*() const;
+
+        const std::pair<Key, Value>* operator->() const;
+
+        ConstIterator operator++();
+        ConstIterator operator++(int);
+
+        ConstIterator operator--();
+        ConstIterator operator--(int);
+
+        bool operator==(const Iterator& other) const;
+        bool operator!=(const Iterator& other) const;
+
+    private:
+        const Node* _node;
+    };
+
+    // вставить элемент с ключем key и значением value
+    void insert(const Key& key, const Value& value);
+
+    // удалить все элементы с ключем key
+    void erase(const Key& key);
+
+    // найти первый элемент в дереве, равный ключу key
+    ConstIterator find(const Key& key) const;
+    Iterator find(const Key& key);
+
+    // найти все элементы, у которых ключ равен key
+    // первый итератор пары - первый элемент в дереве, равный key
+    // второй итератор пары - первый элемент в дереве больший, чем key
+    // [pair.first, pair.second) - полуинтервал, содержащий все элементы с ключем key
+    std::pair<Iterator, Iterator> equalRange(const Key& key);
+    std::pair<ConstIterator, ConstIterator> equalRange(const Key& key) const;
+
+    // получить итератор на минимальное значение в дереве
+    ConstIterator min(const Key& key) const;
+    // получить итератор на максимальное значение в дереве
+    ConstIterator max(const Key& key) const;
+
     Iterator begin();
     Iterator end();
+
+    ConstIterator cbegin() const;
+    ConstIterator cend() const;
+
     size_t size() const;
     void print();
+
 private:
     size_t _size = 0;
-    Node* _root = nullptr;
-    void add(Node* pref, Node* current);
+    Node* _root = nullptr; //!< корневой узел дерева
+};
+
+
+/*!
+    Имплементация словаря
+    Не допускается дублирование ключей (аналог std::map)
+*/
+
+template <typename Key, typename Value>
+class Map
+{
+    BinarySearchTree<Key, Value> _tree;
+public:
+    using MapIterator = typename BinarySearchTree<Key, Value>::Iterator;
+    using ConstMapIterator = typename BinarySearchTree<Key, Value>::ConstIterator;
+
+    Map() = default;
+    
+    explicit Map(const Map& other);
+    Map& operator=(const Map& other);
+
+    explicit Map(Map&& other) noexcept;
+    Map& operator=(Map&& other) noexcept;
+
+    ~Map() = default;
+
+    // вставить элемент с ключем key и значением value
+    // если узел с ключем key уже представлен, то заменить его значение на value
+    void insert(const Key& key, const Value& value);
+
+    // удалить элемент с ключем key
+    void erase(const Key& key);
+
+    // найти элемент, равный ключу key
+    ConstMapIterator find(const Key& key) const;
+    MapIterator find(const Key& key);
+
+    // доступ к элементу по ключу
+    // если в момент обращения элемента не существует, создать его, 
+    // ключ равен key, value равно дефолтному значению для типа Value
+    const Value& operator[](const Key& key) const;
+    Value& operator[](const Key& key);
+
+    MapIterator begin();
+    MapIterator end();
+
+    ConstMapIterator cbegin() const;
+    ConstMapIterator cend() const;
+
+    size_t size() const;
+};
+
+
+/*!
+    Имплементация множества
+    Не допускается дублирование ключей (аналог std::set)
+*/
+template <typename Value>
+class Set
+{
+    Map<Value, Value> _map;
+
+public:
+    using SetIterator = typename Map<Value, Value>::MapIterator;
+    using ConstSetIterator = typename Map<Value, Value>::ConstMapIterator;
+
+    Set() = default;
+
+    explicit Set(const Set& other);
+    Set& operator=(const Set& other);
+
+    explicit Set(Set&& other) noexcept;
+    Set& operator=(Set&& other) noexcept;
+
+    ~Set() = default;
+
+    void insert(const Value& value);
+
+    void erase(const Value& value);
+
+    ConstSetIterator find(const Value& value) const;
+    SetIterator find(const Value& key);
+
+    bool contains(const Value& value) const;
 };
